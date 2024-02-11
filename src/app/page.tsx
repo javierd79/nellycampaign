d47affd0e631fc0795da51205efeba9581330870
Javier Diaz <javierdiazt406@gmail.com>
2024-02-11 15:00:55 -0400

    Add referencial image layer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { MantineProvider } from '@mantine/core';
+import { MantineProvider, Text } from '@mantine/core';
 import Navbar from './components/Navbar';
 
 function App() {
@@ -9,8 +9,12 @@ function App() {
       <MantineProvider withGlobalStyles withNormalizeCSS>
         <div className="container">
           <Navbar />
+
         </div>
         <div className="container-fluid first-section first-section">
+          <div className="container" style={{ borderTop: "2px dashed #3023AE", borderLeft: "2px dashed #3023AE", borderRight: "2px dashed #3023AE", height: 'calc(100vh - 79px)', borderRadius: '7px 7px 0 0' }}>
+            <Text ta="center" fw={550} fz={40} italic>Imagen referencial aquí</Text>
+          </div>
           {/* <FeaturesAsymmetrical /> */}
         </div>
         <div className="container-fluid maxHeight first-sectionnnn">
@@ -26,7 +30,7 @@ function App() {
           {/* <img style={{ position: 'absolute', bottom: 100, right: 300 }} src="/women_dress_red.png" /> */}
         </div>
       </MantineProvider>
-    </main>
+    </main >
   )
 }
 
